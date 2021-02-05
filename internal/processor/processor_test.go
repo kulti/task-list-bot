03f4ee7eeb9ba9f79473975c5eff2795fe1ce92d
Aleksey Bakin <alexey.bakin@gmail.com>
2021-02-05 18:17:31 +0300

    feat: formal code to create sprint

--- internal/processor/processor_test.go
+++ internal/processor/processor_test.go
@@ -1,7 +1,10 @@
 package processor_test
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 
@@ -10,11 +13,29 @@ import (
 
 type ProcessorSuite struct {
 	suite.Suite
+	processor *processor.Processor
+}
+
+func (s *ProcessorSuite) SetupTest() {
+	s.processor = processor.New()
 }
 
 func (s *ProcessorSuite) TestEmptyMessage() {
-	p := processor.New()
-	s.Require().Panics(func() { p.Process("") })
+	s.Require().Panics(func() { s.processor.Process("") })
+}
+
+func (s *ProcessorSuite) TestCreateSprint() {
+	const timeDay = 24 * time.Hour
+	begin := time.Unix(time.Now().Unix()-rand.Int63n(5000)+rand.Int63n(5000), 0).Truncate(timeDay)
+	end := begin.Add(7 * timeDay)
+
+	sprintHeader := fmt.Sprintf("%s - %s", s.timeToSprintDate(begin), s.timeToSprintDate(end))
+	resp := s.processor.Process("/ns " + sprintHeader)
+	s.Require().Equal(sprintHeader, resp)
+}
+
+func (s *ProcessorSuite) timeToSprintDate(d time.Time) string {
+	return fmt.Sprintf("%02d.%02d", d.Day(), d.Month())
 }
 
 func TestProcessorSuite(t *testing.T) {
