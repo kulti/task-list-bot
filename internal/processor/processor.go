72ab7481171ac935360dfec8c06165d5263b47b4
Aleksey Bakin <alexey.bakin@gmail.com>
2021-02-05 18:17:31 +0300

    feat: formal code to create sprint

--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -1,5 +1,7 @@
 package processor
 
+import "strings"
+
 // Processor is a telemgram message processor.
 type Processor struct{}
 
@@ -14,5 +16,28 @@ func (s *Processor) Process(msg string) string {
 		panic("empty message are not allowed")
 	}
 
+	if msg[0] == '/' {
+		return s.processCommand(msg)
+	}
 	return msg
+}
+
+func (s *Processor) processCommand(msg string) string {
+	const msgParts = 2
+	msgSplitted := strings.SplitN(msg, " ", msgParts)
+	if len(msgSplitted) != msgParts {
+		return ""
+	}
+
+	cmd := msgSplitted[0]
+	params := msgSplitted[1]
+	if cmd == "/ns" {
+		return s.processNewSprint(params)
+	}
+
+	return ""
+}
+
+func (s *Processor) processNewSprint(params string) string {
+	return params
 }
