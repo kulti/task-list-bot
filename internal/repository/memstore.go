51d62719c5a77ae1bea552f5fab0137a9e78192a
Aleksey Bakin <alexey.bakin@gmail.com>
2021-03-06 10:19:14 +0300

    refactor: move business logic to store

--- internal/repository/memstore.go
+++ internal/repository/memstore.go
@@ -1,52 +1,23 @@
 package repository
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/kulti/task-list-bot/internal/models"
-)
-
 type Store struct {
-	tasks models.TaskList
+	data []byte
 }
 
 func New() *Store {
 	return &Store{}
 }
 
-func (s *Store) CreateNewSprint(begin, end time.Time) error {
-	s.tasks = models.TaskList{
-		Title: fmt.Sprintf("%s - %s", s.timeToSprintDate(begin), s.timeToSprintDate(end)),
-	}
+func (s *Store) InitNewSprint() error {
+	s.data = nil
 	return nil
 }
 
-func (s *Store) CreateTask(text string, points int) error {
-	id := len(s.tasks.Tasks)
-	s.tasks.Tasks = append(s.tasks.Tasks, models.Task{ID: id, Text: text, Points: models.Points{Total: points}})
-	s.tasks.Points.Total += points
+func (s *Store) CurrentSprint() ([]byte, error) {
+	return s.data, nil
+}
+
+func (s *Store) UpdateCurrentSprint(data []byte) error {
+	s.data = data
 	return nil
-}
-
-func (s *Store) DoneTask(id int) (string, error) {
-	if id >= len(s.tasks.Tasks) {
-		return "", models.ErrTaskNotFound
-	}
-
-	task := s.tasks.Tasks[id]
-	s.tasks.Points.Burnt += task.Points.Total - task.Points.Burnt
-
-	s.tasks.Tasks[id].State = models.TaskStateDone
-	s.tasks.Tasks[id].Points.Burnt = task.Points.Total
-
-	return s.tasks.Tasks[id].Text, nil
-}
-
-func (s *Store) CurrentSprint() (models.TaskList, error) {
-	return s.tasks, nil
-}
-
-func (s *Store) timeToSprintDate(d time.Time) string {
-	return fmt.Sprintf("%02d.%02d", d.Day(), d.Month())
 }
--- internal/store/store.go
+++ internal/store/store.go
@@ -1,20 +1,29 @@
 package store
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/kulti/task-list-bot/internal/models"
 )
 
 type repository interface {
-	CurrentSprint() (models.TaskList, error)
-	CreateNewSprint(begin, end time.Time) error
-	CreateTask(text string, points int) error
-	DoneTask(id int) (string, error)
+	InitNewSprint() error
+	CurrentSprint() ([]byte, error)
+	UpdateCurrentSprint(data []byte) error
 }
 
 type Store struct {
-	repo repository
+	repo  repository
+	tasks taskListHistory
+}
+
+type taskListHistory []*taskListHistoryItem
+
+type taskListHistoryItem struct {
+	Description string
+	models.TaskList
 }
 
 func New(repo repository) *Store {
@@ -24,17 +33,107 @@ func New(repo repository) *Store {
 }
 
 func (s *Store) CurrentSprint() (models.TaskList, error) {
-	return s.repo.CurrentSprint()
+	if err := s.init(); err != nil {
+		return models.TaskList{}, err
+	}
+
+	return s.tasks[len(s.tasks)-1].TaskList, nil
 }
 
 func (s *Store) CreateNewSprint(begin, end time.Time) error {
-	return s.repo.CreateNewSprint(begin, end)
+	s.tasks = []*taskListHistoryItem{{
+		Description: "Sprint is created",
+		TaskList: models.TaskList{
+			Title: fmt.Sprintf("%s - %s", s.timeToSprintDate(begin), s.timeToSprintDate(end)),
+		},
+	}}
+
+	_ = s.repo.InitNewSprint()
+	if err := s.flush(); err != nil {
+		s.tasks = nil
+		return fmt.Errorf("flush task list into db: %w", err)
+	}
+
+	return nil
 }
 
 func (s *Store) CreateTask(text string, points int) error {
-	return s.repo.CreateTask(text, points)
+	if err := s.init(); err != nil {
+		return err
+	}
+
+	histItem := s.dupHistoryItem()
+
+	id := len(histItem.Tasks)
+	task := models.Task{ID: id, Text: text, Points: models.Points{Total: points}}
+	histItem.Tasks = append(histItem.Tasks, task)
+	histItem.Points.Total += points
+
+	return s.putHistoryItem(histItem)
 }
 
 func (s *Store) DoneTask(id int) (string, error) {
-	return s.repo.DoneTask(id)
+	if err := s.init(); err != nil {
+		return "", err
+	}
+
+	histItem := s.dupHistoryItem()
+
+	if id >= len(histItem.Tasks) {
+		return "", models.ErrTaskNotFound
+	}
+
+	histItem.Tasks = append([]models.Task{}, histItem.Tasks...)
+	task := histItem.Tasks[id]
+	histItem.Points.Burnt += task.Points.Total - task.Points.Burnt
+	histItem.Tasks[id].State = models.TaskStateDone
+	histItem.Tasks[id].Points.Burnt = task.Points.Total
+
+	return task.Text, s.putHistoryItem(histItem)
+}
+
+func (s *Store) timeToSprintDate(d time.Time) string {
+	return fmt.Sprintf("%02d.%02d", d.Day(), d.Month())
+}
+
+func (s *Store) dupHistoryItem() *taskListHistoryItem {
+	histItem := *s.tasks[len(s.tasks)-1]
+	return &histItem
+}
+
+func (s *Store) putHistoryItem(histItem *taskListHistoryItem) error {
+	s.tasks = append(s.tasks, histItem)
+	if err := s.flush(); err != nil {
+		s.tasks = s.tasks[:len(s.tasks)-1]
+		return fmt.Errorf("flush task list into db: %w", err)
+	}
+	return nil
+}
+
+func (s *Store) init() error {
+	if len(s.tasks) != 0 {
+		return nil
+	}
+
+	data, err := s.repo.CurrentSprint()
+	if err != nil {
+		return fmt.Errorf("query current sprint: %w", err)
+	}
+
+	var tasksHistory taskListHistory
+	if err := json.Unmarshal(data, &tasksHistory); err != nil {
+		return fmt.Errorf("query current sprint: %w", err)
+	}
+
+	s.tasks = tasksHistory
+	return nil
+}
+
+func (s *Store) flush() error {
+	data, err := json.Marshal(&s.tasks)
+	if err != nil {
+		return fmt.Errorf("marshal tasks: %w", err)
+	}
+
+	return s.repo.UpdateCurrentSprint(data)
 }
