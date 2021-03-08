c2be9ba0dbb7d52bf5074f410b420b4b457f9aa7
Aleksey Bakin <alexey.bakin@gmail.com>
2021-03-08 15:23:31 +0300

    tests: done task and burn points

--- internal/store/bytes_matcher_test.go
+++ internal/store/bytes_matcher_test.go
@@ -1,37 +1,63 @@
 package store_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"reflect"
 )
 
 type bytesMatcher string
 
 func (m bytesMatcher) Matches(x interface{}) bool {
-	actual, ok := x.([]byte)
+	actualBytes, ok := x.([]byte)
 	if !ok {
 		return false
 	}
 
-	actual = append(actual, '\n')
+	var actual interface{}
+	if err := json.Unmarshal(actualBytes, &actual); err != nil {
+		log.Printf("failed unmarshall actual bytes %q: %v", string(actualBytes), err)
+		return false
+	}
 
 	expectedFileName := filepath.Join("testdata", string(m))
 	if update {
-		if err := ioutil.WriteFile(expectedFileName, actual, 0600); err != nil {
+		expectedBytes, err := json.MarshalIndent(&actual, "", "    ")
+		if err != nil {
+			log.Printf("failed marshall actual %+v: %v", actual, err)
+			return false
+		}
+
+		expectedBytes = append(expectedBytes, '\n')
+		if err := ioutil.WriteFile(expectedFileName, expectedBytes, 0600); err != nil {
 			log.Printf("failed to write %q: %v", expectedFileName, err)
 			return false
 		}
+
+		return true
 	}
 
-	expected, err := ioutil.ReadFile(expectedFileName)
+	expectedBytes, err := ioutil.ReadFile(expectedFileName)
 	if err != nil {
 		log.Printf("failed to read %q: %v", expectedFileName, err)
 		return false
 	}
 
-	return string(expected) == string(actual)
+	var expected interface{}
+	if err := json.Unmarshal(expectedBytes, &expected); err != nil {
+		log.Printf("failed unmarshall expected bytes %q: %v", string(expectedBytes), err)
+		return false
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		log.Printf("not equal %q\nExpected: %+v\nActual: %+v\n", expectedFileName, expected, actual)
+		return false
+	}
+
+	return true
 }
 
 func (m bytesMatcher) String() string {
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -1,6 +1,8 @@
 package store_test
 
 import (
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -36,6 +38,49 @@ func (s *StoreSuite) TestCreateNewSprint() {
 	s.mockRepository.EXPECT().UpdateCurrentSprint(bytesMatcher("new_sprint"))
 
 	s.Require().NoError(s.store.CreateNewSprint(begin, end))
+}
+
+func (s *StoreSuite) TestCreateTask() {
+	sprintData, err := ioutil.ReadFile(filepath.Join("testdata", "new_sprint"))
+	s.Require().NoError(err)
+	s.mockRepository.EXPECT().CurrentSprint().Return(sprintData, nil)
+
+	s.mockRepository.EXPECT().UpdateCurrentSprint(bytesMatcher("new_task"))
+
+	s.Require().NoError(s.store.CreateTask("new task", 10))
+}
+
+func (s *StoreSuite) TestDoneTask() {
+	sprintData, err := ioutil.ReadFile(filepath.Join("testdata", "new_task"))
+	s.Require().NoError(err)
+	s.mockRepository.EXPECT().CurrentSprint().Return(sprintData, nil)
+
+	s.mockRepository.EXPECT().UpdateCurrentSprint(bytesMatcher("done_task"))
+
+	task, err := s.store.DoneTask(0)
+	s.Require().NoError(err)
+	s.Require().Equal("new task", task)
+}
+
+func (s *StoreSuite) TestBurnPoints() {
+	sprintData, err := ioutil.ReadFile(filepath.Join("testdata", "burn_init"))
+	s.Require().NoError(err)
+	s.mockRepository.EXPECT().CurrentSprint().Return(sprintData, nil)
+
+	s.mockRepository.EXPECT().UpdateCurrentSprint(bytesMatcher("burn_some_task_0"))
+	task0, err := s.store.BurnTaskPoints(0, 1)
+	s.Require().NoError(err)
+	s.Require().Equal("task 0", task0)
+
+	s.mockRepository.EXPECT().UpdateCurrentSprint(bytesMatcher("burn_overflow_task_1"))
+	task1, err := s.store.BurnTaskPoints(1, 2)
+	s.Require().NoError(err)
+	s.Require().Equal("task 1", task1)
+
+	s.mockRepository.EXPECT().UpdateCurrentSprint(bytesMatcher("burn_done_task_0"))
+	task0, err = s.store.BurnTaskPoints(0, 3)
+	s.Require().NoError(err)
+	s.Require().Equal("task 0", task0)
 }
 
 func TestStore(t *testing.T) {
